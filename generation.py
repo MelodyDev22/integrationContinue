f4ab2c6d5872cdd03d962ad743393b92a649dd67
Mélody <melody.charles@ecoles-epsi.net>
2024-04-04 11:51:37 +0200

    ok

--- generation.py
+++ generation.py
@@ -1,5 +1,7 @@
 fichier = open("index.html","w")
 
-fichier.write("html><body><h1>Site web généré par </h1>")
+fichier.write("html><body><h1>Site web automatique </h1>")
 fichier.write ("<p> ceci est le site du client <br> <p/>")
 fichier.write ("</body></html>")
+
+fichier.close ()
